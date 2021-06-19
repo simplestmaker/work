8d21afae36f4f085e47c89c68d65e49bb338898b
zhaohongbo <63685824+simplestmaker@users.noreply.github.com>
2021-06-19 16:53:23 +0800

    Update typeRouter.js

--- 基于nodejs_es6_mysql的数据接口实现/router/typeRouter.js
+++ 基于nodejs_es6_mysql的数据接口实现/router/typeRouter.js
@@ -1,9 +1,4 @@
-/*
- * @Author: RYPY
- * @Date: 2020-12-03 10:03:36
- * @LastEditors: RYPY
- * @LastEditTime: 2020-12-03 10:03:37
- */
+
 const express=require("express");
 const router=express.Router();
 module.exports=router;
