7676446cc56923c3bea34062406f21b9ee7de855
zhaohongbo <63685824+simplestmaker@users.noreply.github.com>
2021-06-19 16:53:37 +0800

    Update userRouter.js

--- 基于nodejs_es6_mysql的数据接口实现/router/userRouter.js
+++ 基于nodejs_es6_mysql的数据接口实现/router/userRouter.js
@@ -1,9 +1,4 @@
-/*
- * @Author: RYPY
- * @Date: 2020-12-03 10:03:12
- * @LastEditors: RYPY
- * @LastEditTime: 2020-12-06 09:25:29
- */
+
 const express=require("express");
 const userModel=require("../model/UserModel");
 
